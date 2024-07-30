bd393dd64aa4ce0b176fa96af2473cbe0fa2ca27
Priyanka Kataria <priyanka_kataria25@yahoo.co.in>
2024-07-30 23:23:45 +0530

    Mortgage transfer

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,7 +1,7 @@
 
 export const checkIfMortgageExists = (mortgageType:any,item:any,customerData:any) => {
     if(mortgageType ==="inmortgage") {
-        if(item.isMortgage && customerData[0]?.data[0]?.age >= 18 && customerData[1]?.data?.country_of_nationality === "GB") {
+        if(item.isMortgage && customerData[0]?.data[0]?.age > 18 && customerData[1]?.data?.country_of_nationality === "GB") {
             return true;
         } else {
             return false;
